ad0eeb41a4e89609ff5081e9fc3f049a2ab9722c
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-04-06 21:37:49 +0900

    create items controller
    coffee.jsなどのファイルが生成されないようにrails g　しました

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module FreemarketSampleA67
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    
+    config.load_defaults 5.2
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
