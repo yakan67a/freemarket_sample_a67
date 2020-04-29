fca30b14608f5e7869cd105048f017d177f85a0a
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-04-29 17:49:59 +0900

    create-item-table
    itemテーブルを作成しました

---
+++ db/migrate/20200429044917_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :items_name,       null: false
+      t.text   :item_description, null: false
+      t.string :condition,        null: false
+      t.string :shipping_costs,   null: false
+      t.string :days_to_ship,     null: false
+      t.integer :price,           null: false
+      t.integer :user_id,         foreign_key: true
+      t.integer :category_id,     foreign_key: true, null: false
+      t.integer :brand_id,        foreign_key: true
+      t.integer :shipping_area_id, foreign_key: true, null: false
+      t.integer :comment_id,      foreign_key: true
+      t.timestamps
+    end
+  end
+end
