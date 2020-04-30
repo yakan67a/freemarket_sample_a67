c45d69d4eb05039ee561c4255f4994567d8b6b76
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-04-30 17:35:51 +0900

    set new action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,13 @@
 class CardsController < ApplicationController
+  require 'payjp'
+  before_action :set_payjp_key, only: [:create, :show, :destroy]
 
   def new
+    # カード情報を持ってるならカード情報表示のアクションにそのまま渡す
+    card = current_user.card
+    if card.present? && card.card_id.present?
+      redirect_to action: :show
+    end
   end
 
   def create
@@ -12,4 +19,9 @@ class CardsController < ApplicationController
   def destroy
   end
   
+  private
+  def set_payjp_key # payjpの秘密鍵をセットする
+    Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
+  end
+
 end
