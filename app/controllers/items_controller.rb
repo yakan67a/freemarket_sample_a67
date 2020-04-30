ea517548d8fa8d546cb287f94d8c8ab126183248
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-04-30 16:23:18 +0900

    update-items-controller-2020-04-30
    コントローラーにアクションとプライベートメソッドを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,26 @@ def new
   @item = Item.new
   @item.item_images.new
 end
+def create
+  @item = Item.new(item_params)
+  if @item.save!
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+private
+def item_params
+  params.require(:item).permit(:items_name,
+      :item_description,
+      :condition,
+      :shipping_costs,
+      :days_to_ship,
+      :price,
+      :category_id,
+      :brand_id,
+      :shipping_area_id,
+      item_images_attributes: [:image_URL]).merge(user_id: current_user.id)
+end
 end
