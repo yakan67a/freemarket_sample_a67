c8eddf109d974528ee4ea2a9b5456efdbb40a058
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-05-01 18:53:59 +0900

    revert-revert-category
    masterに変更点が反映されていなかったのでもう一度プルリク出します

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   has_many :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true
   has_many   :comments
   belongs_to :category
-  belongs_to :brand
-  belongs_to :prefecture
-  belongs_to :user
+  belongs_to :brand, optional: true
+  belongs_to :user  
   belongs_to :rating, dependent: :destroy
   has_one    :history
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#index"
-  resources :items
+  resources :items  do
+    collection do
+      get 'get_category_children', defaults: {format: 'json'}
+      get 'get_category_grandchildren', defaults: {format: 'json'}
+    end
+  end
+
   resources :users, only: [:index, :show] do
     collection do
       get 'logout'
