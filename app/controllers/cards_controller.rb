b406facdabd54e1f4c19c77c168693d6e1183fcf
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-01 19:21:25 +0900

    create record of card table
    カード情報を登録しようとしたuserが、まだcard table を持っていなかった場合の処理を書きました。
    Payjpの顧客ID作成〜cardのレコード新規作成を行います。

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,6 +10,33 @@ class CardsController < ApplicationController
   end
 
   def create
+    token = params["payjp-token"]
+    if token.blank? # トークンの取得に失敗していたら戻る
+      redirect_to action: :new
+    else
+      if current_user.card.blank?
+        # ユーザーがcardテーブルを持っていない場合の処理
+        customer = Payjp::Customer.create(
+          description: "test", 
+          card: token, 
+          metadata: {user_id: current_user.id}
+          )
+
+        card = Card.new(
+          user_id: current_user.id, 
+          customer_id: customer.id, 
+          card_id: customer.default_card
+          )
+        
+        if card.save
+          redirect_to action: :index
+        else
+          redirect_to action: :new
+        end
+      else
+      # ユーザーがcardテーブルを持っている場合の処理
+      end
+    end
   end
 
   def destroy
