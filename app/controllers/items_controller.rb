ca8953e1562c69b044e2d6a359b5cb4ee2fb5c85
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-05-01 17:15:22 +0900

    create-method-for-ajax
    ajaxで処理するために新規でメソッドを追加しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,9 @@ class ItemsController < ApplicationController
 def new
   @item = Item.new
   @item.item_images.new
+  @category_parent_array = Category.where(ancestry: nil)
 end
+
 def create
   @item = Item.new(item_params)
   if @item.save!
@@ -11,6 +13,14 @@ def create
   else
     render :new
   end
+end
+
+def get_category_children
+  @category_children = Category.find(params[:parent_id]).children
+end
+
+def get_category_grandchildren
+  @category_grandchildren = Category.find(params[:child_id]).children
 end
 
 private
