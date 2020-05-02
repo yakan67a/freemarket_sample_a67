0c08b7f6c6f432901e11c9ba1c104a803a876478
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-02 21:09:37 +0900

    tests of card model
    Cardモデルの単体テストを記述します。

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe Card do
+  describe "#create" do
+    it "user_id, customer_id, card_idが存在すれば登録できること" do
+      user = create(:user)
+      card = build(:card, user_id: user.id)
+      expect(card).to be_valid
+    end
+
+    it "user_idが存在ない場合登録できないこと" do
+      card = build(:card, user_id: "")
+      card.valid?
+      expect(card.errors[:user_id]).to include("を入力してください")
+    end
+
+    it "customer_idがない場合登録できないこと" do
+      user = create(:user)
+      card = build(:card, user_id: user.id, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("を入力してください")
+    end
+    
+    it "card_idがない場合でも登録できること" do
+      user = create(:user)
+      card = build(:card, user_id: user.id, card_id: "")
+      expect(card).to be_valid
+    end
+    
+  end
+end
