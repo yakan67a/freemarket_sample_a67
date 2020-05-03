a8fa7e64754c486d5e68b9637afcb9ff5b3a54f9
sawaken-tech <sawapaca@gmail.com>
2020-05-03 15:11:16 +0900

    view_profile_imge
    アップロードしたプロフィール画像を表示するために
    mypage_image_uoloader.rb作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   validates :name_first, :name_last, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力してください" }
   validates :name_first_kana, :name_last_kana, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナで入力してください" }
   has_one :shipping_address, dependent: :destroy
+
+  mount_uploader :profile_image, MypageImageUploader
 end
