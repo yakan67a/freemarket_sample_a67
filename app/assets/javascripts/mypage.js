79feb31d4e6e08923a937338e434aee6545b5424
sawaken-tech <sawapaca@gmail.com>
2020-05-04 13:15:27 +0900

    add_mypage-preview-image
    マイページ画像のプレビュー表示
    また画像以外のファイルが選択された場合エラーメッセージ表示

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -8,3 +8,21 @@ $(function(){
   }
   tabs.click(tabSwitch);
 });
+
+// プロフィール編集画面での画像プレビュー表示
+$(function(){
+  $('.editProfile__main__imgbox--img').change(function(e){
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    if(file.type.indexOf("image") < 0){
+      $('.editProfile__main__imgbox').append('<div class=file_error>画像ファイルを指定してください</div>')
+      return false;
+    }
+    reader.onload = (function(file){
+      return function(e){
+        $(".profile_image").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  })
+})
