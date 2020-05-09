8d32cf6c830971747027e9ccda585337410c5885
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-05-09 12:32:54 +0900

    test-post-item
    出品機能のテストです

---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  
+  factory :item do
+    items_name            {"靴"}
+    item_description      {"履きやすい"}
+    condition             {"未使用に近い"}
+    shipping_costs        {"送料込み(出品者負担)"}
+    days_to_ship          {"3~4日で発送"}
+    price                 {"400"}
+    category_id           {"280"}
+    brand_id              {"1"}
+    shipping_area_id      {"7"}    
+  end
+end
