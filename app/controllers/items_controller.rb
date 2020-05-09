27d08f002c0275ddef02d78cc93e2fcc1e885699
sawaken-tech <sawapaca@gmail.com>
2020-05-09 16:18:39 +0900

    define_item
    商品編集機能実装に伴うアクションの定義
    index:出品した商品一覧表示用
    edit:出品した商品一覧表示用
    update:出品した商品一覧表示用

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,40 +1,52 @@
 class ItemsController < ApplicationController
 
-def new
-  @item = Item.new
-  @item.item_images.new
-  @category_parent_array = Category.where(ancestry: nil)
-end
-
-def create
-  @item = Item.new(item_params)
-  if @item.save!
-    redirect_to root_path
-  else
-    render :new
+  # 出品した商品一覧表示用
+  def index
   end
-end
+    
+  def new
+    @item = Item.new
+    @item.item_images.new
+    @category_parent_array = Category.where(ancestry: nil)
+  end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-def get_category_children
-  @category_children = Category.find(params[:parent_id]).children
-end
+  # 出品商品編集用
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-def get_category_grandchildren
-  @category_grandchildren = Category.find(params[:child_id]).children
-end
+  # 編集した商品更新用
+  def update
+  end
 
-private
-def item_params
-  params.require(:item).permit(:items_name,
-      :item_description,
-      :condition,
-      :shipping_costs,
-      :days_to_ship,
-      :price,
-      :category_id,
-      :brand_id,
-      :shipping_area_id,
-      item_images_attributes: [:image_URL,:_destroy, :id]).merge(user_id: current_user.id)
-end
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:items_name,
+        :item_description,
+        :condition,
+        :shipping_costs,
+        :days_to_ship,
+        :price,
+        :category_id,
+        :brand_id,
+        :shipping_area_id,
+        item_images_attributes: [:image_URL,:_destroy, :id]).merge(user_id: current_user.id)
+  end
 end
