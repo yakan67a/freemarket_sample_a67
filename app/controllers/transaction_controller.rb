56652250d52d6397b181d8d449149531cd127166
m0nkeywrench <59466775+m0nkeywrench@users.noreply.github.com>
2020-05-09 19:58:18 +0900

    set private method

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,4 +1,7 @@
 class TransactionController < ApplicationController
+  require 'payjp'
+  before_action :move_to_login
+  before_action :set_payjp_key, only: :buy
 
   def buy
     @item = Item.find(params[:item_id])
@@ -13,6 +16,15 @@ class TransactionController < ApplicationController
   end
 
   def error
+  end
+  
+  private
+  def move_to_login
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def set_payjp_key # payjpの秘密鍵をセットする
+    Payjp.api_key = Rails.application.credentials[:payjp][:secret_key]
   end
 
 end
