69bd8e454c8f4a3dd47f3b5aa557a7bbbaa02940
yuku214 <60311206+yuku214@users.noreply.github.com>
2020-05-09 13:18:31 +0900

    fix-validation-price
    より強いValidationを組むことでバグの修正をしました
    
    テストも通りました

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -43,5 +43,20 @@ describe Item do
       item.valid?
       expect(item.errors[:shipping_costs]).to include("を入力してください")
     end
+    it "priceがinteger以外ならNG" do
+      item = build(:item, price: "３００")
+      item.valid?
+      expect(item.errors[:price]).to include("は数値で入力してください")
+    end
+    it "priceが300円未満ならNG" do
+      item = build(:item, price: "290")
+      item.valid?
+      expect(item.errors[:price]).to include("は300以上の値にしてください")
+    end
+    it "priceが9999999円よりも高額ならNG" do
+      item = build(:item, price: "19999999")
+      item.valid?
+      expect(item.errors[:price]).to include("は9999999以下の値にしてください")
+    end
   end
 end
