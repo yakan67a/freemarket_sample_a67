62bcca3fbc96f0b4a0a8d9a84c5844e9ad5e0f03
sawaken-tech <sawapaca@gmail.com>
2020-05-18 13:23:04 +0900

    delete_comment
    不要なコメントが残っていた為削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,31 +43,17 @@ class ItemsController < ApplicationController
   # 編集した商品更新用
   def update
     @item = Item.find(params[:id])
-    # if @item.update(item_params)
-    #   redirect_to update_complete_user_path, notice: 'アイテム更新しました'
-    # else
-    #   flash.now[:error] = '更新失敗'
-    #   render update_complete_user_path
-    # end
 
-    # each do で並べた画像が image
-    # 新しくinputに追加された画像が image_attributes
-    # この二つがない時はupdateしない
     if params[:item].keys.include?("item_image") || params[:item].keys.include?("item_images_attributes") 
       if @item.valid?
         if params[:item].keys.include?("item_image") 
-          # dbにある画像がedit画面で一部削除してるか確認
-          update_images_ids = params[:item][:item_image].values #投稿済み画像 
+          update_images_ids = params[:item][:item_image].values
           before_images_ids = @item.item_images.ids
-          #  商品に紐づく投稿済み画像が、投稿済みにない場合は削除する
-          # @item.item_images.ids.each doで、一つずつimageハッシュにあるか確認。なければdestroy
           before_images_ids.each do |before_img_id|
             ItemImage.find(before_img_id).destroy unless update_images_ids.include?("#{before_img_id}") 
           end
         else
           before_images_ids = @item.item_images.ids
-          # imageハッシュがない = 投稿済みの画像をすべてedit画面で消しているので、商品に紐づく投稿済み画像を削除する。
-          # @item.item_images.destroy = nil と削除されないので、each do で一つずつ削除する
           before_images_ids.each do |before_img_id|
             ItemImage.find(before_img_id).destroy 
           end
@@ -75,7 +61,8 @@ class ItemsController < ApplicationController
         @item.update(item_params)
         redirect_to update_complete_user_path(@item), notice: "商品を更新しました"
       else
-        render 'edit'
+        flash.now[:error] = '更新失敗'
+        render update_complete_user_path
       end
     else
       redirect_back(fallback_location: root_path,flash: {success: '画像がありません'})
